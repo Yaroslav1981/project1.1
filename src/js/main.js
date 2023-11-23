ed65949cc095f74d9c7e7014e9dd6144055e49f2
Yaroslav1981 <kyaraslav@gmail.com>
2023-11-23 01:24:28 +0100

    add sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-//import '../scss/style.scss';
+import '../scss/style.scss';
 //import '../css/styles.min.css';
 let accordions = document.querySelectorAll('.accordion__item');
 console.log(accordions);
@@ -11,3 +11,57 @@ for(let accordion of accordions) {
          accordion.setAttribute("class", "accordion__item");
      });
     }
+
+document.querySelector('#btn-burger').addEventListener('click',(e) => {
+    document.querySelector('.section-header').classList.toggle('section-header--active-nav');
+});
+window.addEventListener('resize',(e) => {
+    document.querySelector('.section-header').classList.remove('section-header--active-nav');
+});
+
+
+
+const swiper1 = new Swiper('.section-hero', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 400,
+  
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    
+  
+    
+  });
+const swiper2 = new Swiper('.slider-blog-wrapper', {
+    // Optional parameters
+    //direction: 'horizontal',
+    loop: true,
+    speed: 400,
+  
+    pagination: {
+        el: '.dots-blog',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+  
+    
+  });
+  const swiper3 = new Swiper('.quotes-slider-container', {
+    // Optional parameters
+    //direction: 'horizontal',
+    loop: true,
+    speed: 400,
+  
+    pagination: {
+        el: '.dots-quots',
+        clickable: true,
+      },
+      
+    
+  });
